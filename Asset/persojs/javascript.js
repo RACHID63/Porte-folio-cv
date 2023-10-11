23f5df5b9a5ed7eab85c0446b510a0503e172256
RACHID LAIEB <rachid.laieb@gmail.com>
2023-10-11 21:59:06 +0200

    add gallery famille  bébé

--- Asset/persojs/javascript.js
+++ Asset/persojs/javascript.js
@@ -1,27 +1,32 @@
 
 
-
-
-
 $(document).ready(function(){
     $('#navbar').load('/asset/layout/navbar.html');
     $('#footer').load('/asset/layout/footer.html');
 });
 
-$(document).ready(function () {
-    $('.gallery-button').click(function () {
+$(window).on('load', function () {
+    var hash = window.location.hash.slice(1); // Utilisez slice au lieu de substr
+    if (hash) {
+        // Utilisez trigger('click') au lieu de click()
+        $('.gallery-button[data-gallery="' + hash + '"]').trigger('click');
+    }
+
+    $('.gallery-button').on('click', function (event) {
+        event.stopPropagation();
         var gallery = $(this).data('gallery');
         $('.gallery').hide();
         $('#' + gallery + '-gallery').show();
     });
-});
-
-$(document).ready(function(){
-    $('.gallery-button').click(function(){
+    
+    $('.gallery-button').on('click', function(event){
+        event.stopPropagation();
         $('.gallery-button').removeClass('btn-active');
         $(this).addClass('btn-active');
     });
+    
 });
+
 
 window.onload = function() {
     var navbar = document.getElementById('navbar');
